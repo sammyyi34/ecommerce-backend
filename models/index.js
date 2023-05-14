4f616fb76aba6651eb85af3d7257c5339d87c47c
Sammy <sammyhyi@gmail.com>
2023-05-14 16:27:07 -0400

    updated server.js file

--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo
 // Categories have many Products
 
 // Products belongToMany Tags (through ProductTag)
