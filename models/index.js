f9f13cef5d9a46a8534d2667c2671001170a867e
Sammy <sammyhyi@gmail.com>
2023-05-14 17:46:01 -0400

    made category model

--- models/index.js
+++ models/index.js
@@ -11,7 +11,7 @@ Product.belongsTo(Category, {
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id',
-  onDelete: 'SET NULL',
+  onDelete: 'CASCADE',
 });
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
