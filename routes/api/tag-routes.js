3b3fe797a276bb4a9c35dda9913e0c916d6693f2
Sammy <sammyhyi@gmail.com>
2023-05-14 21:58:41 -0400

    finished

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -21,7 +21,7 @@ router.get('/:id', async (req, res) => {
       include: [{ model: Product }]
     });
     if (!tagData) {
-      res.status(404).json({ message: 'No category found with this id!' });
+      res.status(404).json({ message: 'No tag found with this id!' });
       return;
     }
     res.status(200).json(tagData);
