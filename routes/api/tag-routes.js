ef4d7370923892acc7719bb55b0a36e565eaeced
Sammy <sammyhyi@gmail.com>
2023-05-14 20:29:55 -0400

    made put request for tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -30,6 +30,7 @@ router.get('/:id', async (req, res) => {
   }
 });
 
+// add new tag
 router.post('/', async (req, res) => {
   try {
     const tagData = await Tag.create(req.body);
@@ -39,8 +40,22 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+// update tag
+router.put('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!tagData[0]) {
+      res.status(404).json({ message: 'No tag found with this id!' });
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
